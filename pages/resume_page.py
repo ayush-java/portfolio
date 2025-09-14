c9c14cd53158e89a1de1ed1378dcd86077348223
ayush-java <53033645+ayush-java@users.noreply.github.com>
2025-09-14 11:48:43 -0500

    Resume pdf removed.

--- pages/resume_page.py
+++ pages/resume_page.py
@@ -8,10 +8,12 @@ st.divider()
 resume_path = os.path.join("static", "LatestResume_AyushVelhal.pdf")
 
 # Display PDF inline using an iframe
-with open(resume_path, "rb") as f:
-    base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="900" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
+# with open(resume_path, "rb") as f:
+#     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="900" type="application/pdf"></iframe>'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
+
+# st.pdf("static/LatestResume_AyushVelhal.pdf", height=600)
 
 # Download button
 with open(resume_path, "rb") as f:
